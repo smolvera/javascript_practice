a0f9e97db9bab22af3ed863fbcd64b91d4da368b
Samantha Olvera <smolvera2010@yahoo.com>
2019-08-19 13:59:26 -0400

    booleans

---
+++ booleans.js
@@ -0,0 +1,9 @@
+// --------- Scrimba Introduction to Javascript by Dylan Israel ---------- //
+// 4. Booleans
+/*
+- booleans return true or false value
+*/ 
+
+let map  = true;
+
+console.log(map);
