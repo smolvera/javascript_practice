b532a794f1dccf9c9b77773047638ebbd69472da
Samantha Olvera <smolvera2010@yahoo.com>
2019-08-19 20:00:54 -0400

    changes to index.html and main.js

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 // --------- Scrimba Introduction to Javascript by Dylan Israel ---------- //
 // Place code snippets below to see the magic in the console 
 
-let firstName = "Samantha";
-let lastName = "Olvera";
-
-let course = "Introduction to Javascript";
-let courseSite = "www.scrimba.com";
-
-
